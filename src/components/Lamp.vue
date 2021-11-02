1d11e014d3c2464b6c5e22c3b21f139ee7796bfa
Spokik <Tackesi@yandex.ru>
2021-11-02 21:31:03 +0300

    Вроде все

--- src/components/Lamp.vue
+++ src/components/Lamp.vue
@@ -124,7 +124,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .green-lamp {
   text-align: center;
--- src/store/index.js
+++ src/store/index.js
@@ -32,35 +32,10 @@ export default new Vuex.Store({
       }
     ],
     Loop: null, // итератор цикла светофора
-    nowSignal: null, // обЪект активного на данный момент сигнала
-    redLight: false,
-    yellowLight: false,
-    greenLight: false
+    nowSignal: null // обЪект активного на данный момент сигнала
   },
   mutations: {
-    onRed (state) {
-      this.state.redLight = true
-    },
-    onYelloe (state) {
-      this.state.yellowLight = true
-    },
-    onGreen (state) {
-      this.state.greenLight = true
-    },
-    offRed (state) {
-      this.state.redLight = false
-    },
-    offYelloe (state) {
-      this.state.yellowLight = false
-    },
-    offGreen (state) {
-      this.state.greenLight = false
-    },
-    offAlllamp (state) {
-      this.state.redLight = false
-      this.state.yellowLight = false
-      this.state.greenLight = false
-    },
+
     SelectLoop (state, index) {
       this.state.Loop = index
     },
@@ -75,32 +50,7 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    activeLamp ({ commit, dispatch }, light) {
-      if (light === 'RED') {
-        commit('onRed')
-      } else if (light === 'YELLOW') {
-        commit('onYelloe')
-      } else if (light === 'GREEN') {
-        commit('onGreen')
-      } else {
-        commit('offAlllamp')
-      }
-      dispatch('disableOtherLamp', light)
-    },
-    disableOtherLamp ({ commit }, light) {
-      if (light === 'RED') {
-        commit('offGreen')
-        commit('offYelloe')
-      } else if (light === 'YELLOW') {
-        commit('offGreen')
-        commit('offRed')
-      } else if (light === 'GREEN') {
-        commit('offYelloe')
-        commit('offRed')
-      } else {
-        commit('offAlllamp')
-      }
-    },
+
     SelectLoop ({ commit }, index) {
       commit('SelectLoop', index)
     },
@@ -112,15 +62,13 @@ export default new Vuex.Store({
     },
     openRout ({ commit, dispatch }, { index, timeout }) {
       dispatch('firstStart', index)
-      const iterationArr = this.state.cycle[this.state.Loop]
-      // dispatch('activeLamp', iterationArr.flag)
       setTimeout(() => {
         if (this.state.Loop < 3) {
           commit('plusLoop')
         } else {
           commit('destroyLoop')
         }
-      }, timeout || iterationArr.timeout)
+      }, timeout || this.nowSignal.timeout)
       commit('setNowSignal')
     },
     saveStateOnLocal ({ commit, dispatch }, data) {
